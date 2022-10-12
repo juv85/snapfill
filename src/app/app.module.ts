32fb707baca07490ba90e4b2f5172ffa99b7f955
juv85 <mangajuvet87@gmail.com>
2022-10-12 14:06:49 +0200

    Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
 import { FaceSnapListComponent } from './face-snap-list/face-snap-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FaceSnapComponent,
     FaceSnapListComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule
